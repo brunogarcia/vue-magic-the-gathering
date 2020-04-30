6adbe18491797d584d6535043f4b50c097fa1cb8
Bruno Garcia <bruno@garciaechegaray.com>
2020-04-30 19:20:54 +0200

    Add unit tests for the mutations of the voices store

--- tests/unit/store/voices/mutations.spec.js
+++ tests/unit/store/voices/mutations.spec.js
@@ -9,6 +9,8 @@ const {
   SAVE_ALL_VOICES,
   SAVE_RANDOM_PLAYING_VOICE,
   SAVE_PLAYING_VOICE,
+  TOGGLE_PLAY_VOICE,
+  TOGGLE_FAVOURITE_VOICE,
 } = types;
 
 describe('Mutations', () => {
@@ -46,17 +48,17 @@ describe('Mutations', () => {
 
   it('Save voices', () => {
     const expected = [
-      { id: 123 },
-      { id: 456 },
-      { id: 789 },
+      { id: '123' },
+      { id: '456' },
+      { id: '789' },
     ];
 
     const state = { all: [], cache: [] };
 
     mutations[SAVE_ALL_VOICES](state, [
-      { id: 123 },
-      { id: 456 },
-      { id: 789 },
+      { id: '123' },
+      { id: '456' },
+      { id: '789' },
     ]);
 
     expect(state.all).toEqual(expected);
@@ -65,23 +67,107 @@ describe('Mutations', () => {
 
   it('Save random playing voice', () => {
     const state = {
-      playingId: 456,
+      playingId: '456',
       all: [
-        { id: 123, playing: false },
-        { id: 456, playing: true },
+        { id: '123', playing: false },
+        { id: '456', playing: true },
       ],
     };
 
     mutations[SAVE_RANDOM_PLAYING_VOICE](state);
 
-    expect(state.playingId).toEqual(123);
+    expect(state.playingId).toEqual('123');
   });
 
   it('Save playing voice', () => {
     const state = { playingId: null };
 
-    mutations[SAVE_PLAYING_VOICE](state, 123);
+    mutations[SAVE_PLAYING_VOICE](state, '123');
 
-    expect(state.playingId).toBe(123);
+    expect(state.playingId).toBe('123');
+  });
+
+  it('Toggle play voice: all list', () => {
+    const expected = {
+      all: [
+        { id: '123', playing: false },
+        { id: '456', playing: false },
+      ],
+      cache: [
+        { id: '789', playing: false },
+        { id: '135', playing: false },
+      ],
+    };
+
+    const state = {
+      playingId: '456',
+      all: [
+        { id: '123', playing: false },
+        { id: '456', playing: true },
+      ],
+      cache: [
+        { id: '789', playing: false },
+        { id: '135', playing: false },
+      ],
+    };
+
+    mutations[TOGGLE_PLAY_VOICE](state);
+
+    expect(state.all).toEqual(expected.all);
+    expect(state.cache).toEqual(expected.cache);
+  });
+
+  it('Toggle play voice: cache list', () => {
+    const expected = {
+      all: [
+        { id: '123', playing: false },
+        { id: '456', playing: false },
+      ],
+      cache: [
+        { id: '789', playing: false },
+        { id: '135', playing: false },
+      ],
+    };
+
+    const state = {
+      playingId: '135',
+      all: [
+        { id: '123', playing: false },
+        { id: '456', playing: false },
+      ],
+      cache: [
+        { id: '789', playing: false },
+        { id: '135', playing: true },
+      ],
+    };
+
+    mutations[TOGGLE_PLAY_VOICE](state);
+
+    expect(state.all).toEqual(expected.all);
+    expect(state.cache).toEqual(expected.cache);
+  });
+
+  it('Toggle favourite voice', () => {
+    const id = '789';
+
+    const expected = [
+      { id: '123', favourite: false },
+      { id: '456', favourite: false },
+      { id: '789', favourite: true },
+      { id: '159', favourite: false },
+    ];
+
+    const state = {
+      all: [
+        { id: '123', favourite: false },
+        { id: '456', favourite: false },
+        { id: '789', favourite: false },
+        { id: '159', favourite: false },
+      ],
+    };
+
+    mutations[TOGGLE_FAVOURITE_VOICE](state, id);
+
+    expect(state.all).toEqual(expected);
   });
 });
